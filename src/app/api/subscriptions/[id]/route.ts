cb335343be4288b7b5dabc69522d573efe8e29b7
new-umechan <ryu.shamoji@gmail.com>
2025-10-08 19:47:31 +0900

    add gen-idtoken.ts

---
+++ src/app/api/subscriptions/[id]/route.ts
@@ -0,0 +1,48 @@
+// src/app/api/subscriptions/[id]/route.ts
+// うめ
+
+import { NextRequest, NextResponse } from 'next/server';
+import { withAuth } from "@/lib/middleware";
+import { getFirestore } from 'firebase-admin/firestore'
+const db = getFirestore();
+
+export const PATCH = withAuth(async (
+	req: NextRequest,
+	uid: string,
+	context : { params: { id: string } } // 本来は第二引数、withAuthで第二にしているため第三になっている
+) => {
+	try {
+		const itemId = context.params.id;
+		// jsonにしているが、quantityのみ
+		const { quantity } = await req.json();
+
+		const ref = db.collection('users').doc(uid).collection('cart').doc(itemid);
+
+		// patch
+		await updateDoc(docRef, {
+			quantity : quantity,
+		});
+
+		return NextResponse.json({ msg: "success"})
+	} catch (error) {
+		return NextResponse.json({ error: ""}, { status: 500 });
+	}
+});
+
+export const DELETE = withAuth(async (
+	_req: NextRequest,
+	uid: string,
+	{ params }: { params: { id: string } }
+) => {
+	try {
+		const itemId = context.params.id;
+
+		const ref = db.collection('users').doc(uid).collection('cart').doc(itemid);
+
+		await ref.update({ quantity });
+
+		return NextResponse.json({ msg: "success"})
+	} catch (error) {
+		return NextResponse.json({ error: ""}, { status: 500 });
+	}
+});
---
+++ src/lib/apiUtils.ts
@@ -0,0 +1,56 @@
+// apiで使う、共通処理をまとめとくよ
+// うめ
+
+import foodData from '@/data/foodData.json'
+import { getFirestore, FieldValue } from 'firebase-admin/firestore'
+const db = getFirestore();
+
+// users/{uid}/collecitonの中の全てのドキュメント取得
+export function getCollection (uid:string, collection: string) {
+	const snapshot = await db
+		.collection("users")
+		.doc(uid)
+		.collection(collection)
+		.get();
+
+	return snapshot.docs.map((doc) => ({
+		id: doc.id,
+		...doc.data()
+	}));
+}
+
+// name, price, imgUrlをfoodData.jsonから加える関数
+export function addFoodDetails<T extends { id: string }>(items: T[]) {
+	// name, price, imgUrlをfoodData.jsonから加える
+	return mergedItems = items.map(item => {
+		const food = foodData.find(f => f.id === item.id);
+		return food
+			? { ...item, name: food.name, price: food.priceTax, imgUrl: food.url }
+			: { ...item, name: "no Name", price: 0, imgUrl: "" };
+	});
+}
+
+// postKey
+export function postCollection (
+	uid:string,
+	colleciton: string,
+	items: Array<{ id:string; quantity: number }>
+) {
+	postKey(uid, cart);
+	const batch = db.batch();
+	const cartRef = db.collection("users").doc(uid).collection("cart");
+
+	// idとquantityのみ追加
+	for (const item of items) {
+		const docRef = cartRef.doc(item.id);
+		batch.set(
+			docRef,
+			{ quantity: FieldValue.increment(item.quantity) }
+		)
+	}
+	await batch.commit();
+}
+
+// 
+export function patchCollection(params:type) {
+}
